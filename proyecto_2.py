bd4ddf64bf462166853bed2b47abfcc6f3854702
Carlos Peñas <penasrecinos@gmail.com>
2018-05-09 00:11:47 -0600

    Ya se abren archivos

--- proyecto_2.py
+++ proyecto_2.py
@@ -4,9 +4,6 @@ from tkinter import *
 import tkinter as tk
 import stat
 from tkinter import messagebox
-import traceback
-
-home = "C:\\"
 
 def has_hidden_attribute(filepath):
     return bool(os.stat(filepath).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
@@ -32,6 +29,12 @@ def CurSelet(event):
 def show_error(self, *args):
     messagebox.showerror('Exception',"No ha seleccionado nada")
 
+def ejecutarArchivo(archivo):
+    if os.path.isfile(archivo):
+        os.startfile(archivo)
+    else:
+        messagebox.showinfo("Error","No ha seleccionado un archivo")
+
 def entrarEnDirectorio(directorio):
     for child in cRight.winfo_children():
         child.destroy()
@@ -40,22 +43,25 @@ def entrarEnDirectorio(directorio):
     except:
         messagebox.showinfo("Error","No ha seleccionado una carpeta")
     directorios = noHiddenDir()
-    lista = Listbox(cRight,height=34,fg="black",bg="#ada6a6",selectmode=SINGLE,
-    bd=0,width=73,font=("Arial","12"))
+    lista = Listbox(cRight,height=34,fg="black",bg="#ada6a6",bd=0,width=73,font=("Arial","12"))
     scroll = Scrollbar(cRight, orient=VERTICAL,command=lista.yview)
     scroll.pack(side=RIGHT, fill=Y)
     lista.config(yscrollcommand=scroll.set)
     cwd = Label(cRight, text=os.getcwd(), bg="#ada6a6", font=("Arial", "12")).place(x=210, y=10)
+    acciones = Label(cLeft, text="Acciones", bg="#ada6a6", font=("Arial", "12")).place(x=15, y=70)
+    ejecutar = Button(cLeft,text="Ejecutar archivo",bg="#ada6a6",command = lambda: ejecutarArchivo(os.path.join(os.getcwd()
+    ,lista.get("active"))),font=("Arial", "12"))
     abrir = Button(cLeft,text="Abrir carpeta",bg="#ada6a6",command = lambda: entrarEnDirectorio(os.path.join(os.getcwd()
     ,lista.get("active"))), font=("Arial", "12"))
     tk.Tk.report_callback_exception = show_error
     if type(directorios) == list:
         for i in range(len(directorios)):
             lista.insert(i,directorios[i])
-            abrir.place(x=55, y=35, height=30)
+            abrir.place(x=15, y=95, height=30)
     else:
         lista.insert(1,directorios)
         abrir.destroy()
+    ejecutar.place(x=15,y=130)
     lista.bind('<<ListboxSelect>>',CurSelet)
     lista.place(x=210,y=40)
 
@@ -65,14 +71,13 @@ root.minsize(height=700,width=900)
 root.resizable(width=False,height=False)
 root.title("File Explorer")
 
-
 cRight = Canvas(root,bg="#ada6a6",width=700)
 cRight.place(width=900,height=700)
 cLeft = Canvas(root,bg="#ada6a6",width=200).place(width=200,height=700)
-dir = Label(cLeft,text="Unidades lógicas",bg="#ada6a6",font=("Arial","12")).place(x=10,y=10)
-homeBoton= Button(cLeft,text=home,bg="#ada6a6",command= lambda: entrarEnDirectorio(home),font=("Arial","12")).place(x=15,y=35,height=30)
+unidadesLogicas = Label(cLeft,text="Unidades lógicas",bg="#ada6a6",font=("Arial","12")).place(x=10,y=10)
+homeBoton= Button(cLeft,text="C:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("C:\\"),font=("Arial","12")).place(x=15,y=35,height=30)
 if os.listdir("D:\\"):
-    botonD = Button(cLeft,text="D:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("D:\\"),font=("Arial","12")).place(x=15,y=70,height=30)
+    botonD = Button(cLeft,text="D:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("D:\\"),font=("Arial","12")).place(x=55,y=35,height=30)
 
 
 
