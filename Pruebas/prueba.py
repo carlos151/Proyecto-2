40068de66bc0e1694c88ff3f41f7e9adbbed4a85
Carlos Peñas <penasrecinos@gmail.com>
2018-05-08 19:42:08 -0600

    Cambio de discos

--- Pruebas/prueba.py
+++ Pruebas/prueba.py
@@ -2,41 +2,53 @@ import os
 import os.path
 from tkinter import *
 import tkinter as tk
-from pathlib import Path
+import stat
 
+home = "C:\\"
 
-home = str(Path.home())
-os.chdir(home)
+def has_hidden_attribute(filepath):
+    return bool(os.stat(filepath).st_file_attributes & stat.FILE_ATTRIBUTE_HIDDEN)
 
 def noHiddenDir():
     dir = os.listdir()
     hidden = []
     for elemento in dir:
-        if elemento[0] == ".":
+        if has_hidden_attribute(os.path.abspath(elemento)):
             hidden.append(elemento)
     for elemento in hidden:
         dir.remove(elemento)
     return dir
 
-def botonHome():
+def entrarEnDirectorio(directorio):
+    for child in cRight.winfo_children():
+        child.destroy()
+    scroll = Scrollbar(cRight, orient="vertical", jump=True)
+    scroll.pack(side=RIGHT, fill=Y)
+    os.chdir(directorio)
     directorios = noHiddenDir()
-    y = 143
-    for elemento in directorios:
-        if os.path.isdir(home + "/" + elemento):
-            color = "#fff5a0"
-        else:
-            color = "#c0e7f9"
-        boton=Button(c,text=elemento,bg=color).place(x=30,y=y)
-        y += 30
+    lista = Listbox(cRight,height=len(directorios),fg="black",bg="#ada6a6",selectmode=SINGLE,
+    yscrollcommand=scroll.set,bd=0,width=30)
+    for i in range(len(directorios)):
+        lista.insert(i+1," "+directorios[i])
+    lista.place(x=210,y=40)
+    scroll.config(command=lista.yview)
+    cwd = Label(cRight, text=os.getcwd(), bg="#ada6a6").place(x=210, y=10)
+
 #GUI
 root = tk.Tk()
-root.minsize(900,700)
+root.minsize(height=700,width=900)
+root.resizable(width=False,height=False)
 root.title("File Explorer")
 
-marco = Frame(root,width=200,height=700).place(anchor="e")
-c = Canvas(marco,bg="#ada6a6").place(width=200,height=700)
-dir = Label(c,text="Directorios",bg="#ada6a6").place(x=10,y=85)
-homeBoton= Button(c,text=home,bg="#fff5a0",command= lambda: botonHome()).place(x=15,y=110,height=30)
+cRight = Canvas(root,bg="#ada6a6",width=700)
+cRight.place(width=900,height=700)
+cLeft = Canvas(root,bg="#ada6a6",width=200).place(width=200,height=700)
+scroll = Scrollbar(cRight,orient="vertical",jump=True)
+scroll.pack(side=RIGHT,fill= Y)
+dir = Label(cLeft,text="Unidades lógicas",bg="#ada6a6").place(x=10,y=10)
+homeBoton= Button(cLeft,text=home,bg="#ada6a6",command= lambda: entrarEnDirectorio(home)).place(x=15,y=30,height=30)
+if os.listdir("D:\\"):
+    botonD = Button(cLeft,text="D:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("D:\\")).place(x=15,y=65,height=30)
 
 
 root.mainloop()
