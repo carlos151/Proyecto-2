914baabe47f143670f7dd728e5c5533a50502c42
Carlos Peñas <penasrecinos@gmail.com>
2018-05-09 20:21:05 -0600

    Se han añadido botones sin uso y tamaño de archivos

--- proyecto_2.py
+++ proyecto_2.py
@@ -29,41 +29,101 @@ def CurSelet(event):
 def show_error(self, *args):
     messagebox.showerror('Exception',"No ha seleccionado nada")
 
+def convert_bytes(num):
+    for x in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if num < 1024.0:
+            return "%3.1f %s" % (num, x)
+        num /= 1024.0
+
 def ejecutarArchivo(archivo):
     if os.path.isfile(archivo):
         os.startfile(archivo)
     else:
         messagebox.showinfo("Error","No ha seleccionado un archivo")
 
-def entrarEnDirectorio(directorio):
-    for child in cRight.winfo_children():
+def file_size(file_path):
+    if os.path.isfile(file_path):
+        file_info = os.stat(file_path)
+        return convert_bytes(file_info.st_size)
+    else:
+        return " "
+
+def obtenerTamaños(directorios):
+    resultado = []
+    for elemento in directorios:
+        path = os.path.join(os.getcwd(),elemento)
+        size = file_size(path)
+        resultado.append(size)
+    return resultado
+
+def entrarEnDirectorio(directorio,ord="n"):
+    for child in cRight.winfo_children():#Borrar directorios anteriores de la ventana
         child.destroy()
     try:
         os.chdir(directorio)
     except:
         messagebox.showinfo("Error","No ha seleccionado una carpeta")
+
     directorios = noHiddenDir()
-    lista = Listbox(cRight,height=34,fg="black",bg="#ada6a6",bd=0,width=73,font=("Arial","12"))
-    scroll = Scrollbar(cRight, orient=VERTICAL,command=lista.yview)
+    if type(directorios) == list: #ordenar directorios
+        if ord == "n":
+            directorios.sort(key = lambda k : k.lower())
+
+    for elemento in directorios:#separar carpetas de archivos
+        if os.path.isdir(os.path.join(os.getcwd(),elemento)) == False:
+            directorios.remove(elemento)
+            directorios.append(elemento)
+    tamaños = obtenerTamaños(directorios)
+
+    #listas de directorios,tamaños,etc
+    lista = Listbox(cRight,height=34,fg="black",bg="#ada6a6",bd=0,width=30,font=("Arial","12"))
+    listaTamaño = Listbox(cRight,height=34,fg="black",bg="#ada6a6",bd=0,width=8,font=("Arial","12"))
+    cwd = Label(cRight, text=os.getcwd(), bg="#ada6a6", font=("Arial", "12")).place(x=210, y=10)
+    acciones = Label(cLeft, text="Acciones", bg="#ada6a6", font=("Arial", "12")).place(x=6, y=70)
+    ordenar = Label(cLeft, text="Ordenar por", bg="#ada6a6", font=("Arial", "12")).place(x=6, y=310)
+    scroll = Scrollbar(cRight, orient=VERTICAL)
     scroll.pack(side=RIGHT, fill=Y)
     lista.config(yscrollcommand=scroll.set)
-    cwd = Label(cRight, text=os.getcwd(), bg="#ada6a6", font=("Arial", "12")).place(x=210, y=10)
-    acciones = Label(cLeft, text="Acciones", bg="#ada6a6", font=("Arial", "12")).place(x=15, y=70)
+    listaTamaño.config(yscrollcommand=scroll.set)
+
+    scroll = Scrollbar(cRight, orient=VERTICAL)
+    scroll.config(command = lista.yview())
+
+    #Botones de acciones y ordenamientos
     ejecutar = Button(cLeft,text="Ejecutar archivo",bg="#ada6a6",command = lambda: ejecutarArchivo(os.path.join(os.getcwd()
     ,lista.get("active"))),font=("Arial", "12"))
+    ejecutar.place(x=8, y=128,width=185)
     abrir = Button(cLeft,text="Abrir carpeta",bg="#ada6a6",command = lambda: entrarEnDirectorio(os.path.join(os.getcwd()
     ,lista.get("active"))), font=("Arial", "12"))
-    tk.Tk.report_callback_exception = show_error
+    crearDir = Button(cLeft,text="Crear directorio",bg="#ada6a6",font=("Arial", "12"))
+    crearDir.place(x=8,y=163,width=185)
+    consultarInfo = Button(cLeft,text="Consultar información",bg="#ada6a6",font=("Arial", "12"))
+    consultarInfo.place(x=8,y=198,width=185)
+    eliminar = Button(cLeft,text="Eliminar",bg="#ada6a6",font=("Arial", "12"))
+    eliminar.place(x=8,y=234,width=185)
+    copiar = Button(cLeft,text="Copiar",bg="#ada6a6",font=("Arial", "12"))
+    copiar.place(x=8,y=270,width=185)
+    nombre = Button(cLeft,text="Nombre",bg="#ada6a6",command= lambda : entrarEnDirectorio(directorio),font=("Arial", "12"))
+    nombre.place(x=8,y=335,width=73)
+    tamaño = Button(cLeft, text="Tamaño", bg="#ada6a6",command= lambda : entrarEnDirectorio(directorio,"t"), font=("Arial", "12"))
+    tamaño.place(x=8, y=370,width=73)
+    tipo = Button(cLeft, text="Tipo", bg="#ada6a6",command= lambda : entrarEnDirectorio(directorio,""), font=("Arial", "12"))
+    tipo.place(x=8, y=405,width=73)
+
+    #llenar listas
     if type(directorios) == list:
         for i in range(len(directorios)):
             lista.insert(i,directorios[i])
-            abrir.place(x=15, y=95, height=30)
+            abrir.place(x=8, y=95,width=185)
     else:
         lista.insert(1,directorios)
         abrir.destroy()
-    ejecutar.place(x=15,y=130)
+    for i in range(len(tamaños)):
+        listaTamaño.insert(i,tamaños[i])
     lista.bind('<<ListboxSelect>>',CurSelet)
     lista.place(x=210,y=40)
+    listaTamaño.place(x=470,y=40)
+    tk.Tk.report_callback_exception = show_error
 
 #GUI
 root = tk.Tk()
@@ -74,11 +134,11 @@ root.title("File Explorer")
 cRight = Canvas(root,bg="#ada6a6",width=700)
 cRight.place(width=900,height=700)
 cLeft = Canvas(root,bg="#ada6a6",width=200).place(width=200,height=700)
-unidadesLogicas = Label(cLeft,text="Unidades lógicas",bg="#ada6a6",font=("Arial","12")).place(x=10,y=10)
-homeBoton= Button(cLeft,text="C:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("C:\\"),font=("Arial","12")).place(x=15,y=35,height=30)
+unidadesLogicas = Label(cLeft,text="Unidades lógicas",bg="#ada6a6",font=("Arial","12")).place(x=5,y=10)
+homeBoton= Button(cLeft,text="C:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("C:\\"),font=("Arial","12")).place(x=8,y=35,height=30)
 if os.listdir("D:\\"):
-    botonD = Button(cLeft,text="D:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("D:\\"),font=("Arial","12")).place(x=55,y=35,height=30)
-
+    botonD = Button(cLeft,text="D:\\",bg="#ada6a6",command= lambda: entrarEnDirectorio("D:\\"),font=("Arial","12")).place(x=48,y=35,height=30)
+propiedades = Button(cLeft,text="Propiedades",bg="#ada6a6",font=("Arial", "12")).place(x=88,y=35,height=30)
 
 
 root.mainloop()
